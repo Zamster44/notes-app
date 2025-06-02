3a5ccdc45be4f8fa047d7ed247f4daaa72d1f5f3
Zameer <zameersid10@gmail.com>
2025-06-02 12:56:52 +0530

    updates

--- FrontEnd/src/pages/Login/Login.jsx
+++ FrontEnd/src/pages/Login/Login.jsx
@@ -1,9 +1,9 @@
-import { Navbar } from "../../components/Navbar/Navbar"
-import { Link , useNavigate } from "react-router-dom"
-import { PasswordInput } from "../../components/Input/PasswordInput"
-import { useState } from "react"
-import { validateEmail } from "../../utils/helper"
-import axoisInstance from "../../utils/axoisInstance"
+import { Navbar } from "../../components/Navbar/Navbar";
+import { Link, useNavigate } from "react-router-dom";
+import { PasswordInput } from "../../components/Input/PasswordInput";
+import { useState } from "react";
+import { validateEmail } from "../../utils/helper";
+import axoisInstance from "../../utils/axoisInstance";
 
 export const Login = () => {
   const navigate = useNavigate();
@@ -15,45 +15,50 @@ export const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    if(!validateEmail(email)){
-      setError("Please enter a valid email")
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email");
       return;
     }
 
-    if(!password){
-      setError("Please enter password")
-      return
+    if (!password) {
+      setError("Please enter password");
+      return;
     }
 
-    setError("")
+    setError("");
 
     //login api
     try {
-      const response = await axoisInstance.post("/login" , {
-        email : email,
-        password: password
-      })
+      const response = await axoisInstance.post("/login", {
+        email: email,
+        password: password,
+      });
 
-      if(response.data && response.data.accessToken){
-        localStorage.setItem("token" , response.data.accessToken)
-        navigate('/dashboard')
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        navigate("/dashboard");
       }
     } catch (error) {
-      if(error.response && error.response.data && error.response.data.message){
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
         setError(error.response.data.message);
-      } else{
-        setError("An unexpected error occured")
+      } else {
+        setError("An unexpected error occured");
       }
     }
-  }
+  };
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+      <div className="bg-white flex justify-between px-4 py-2 drop-shadow h-[3.75rem]">
+        <h2 className="text-xl font-medium py-2">Notes</h2>
+      </div>
       <div className="flex-grow flex items-center justify-center">
         <div className="w-96 border rounded bg-white px-7 py-10">
           <form onSubmit={handleLogin}>
-
             <div className="text-2xl mb-7">Login</div>
 
             <input
@@ -66,16 +71,23 @@ export const Login = () => {
 
             <PasswordInput
               value={password}
-              onChange={(e) => {setPassword(e.target.value)}}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
 
             {error && <p className="text-red-500 text-xs pb-1 ">{error}</p>}
 
-            <button type="submit" className="btn-primary">Login</button>
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
 
             <p className="text-sm text-center mt-4">
-              Not Registered Yet? {" "}
-              <Link to={"/signup"} className="font-medium text-primary underline">
+              Not Registered Yet?{" "}
+              <Link
+                to={"/signup"}
+                className="font-medium text-primary underline"
+              >
                 Create an Account
               </Link>
             </p>
@@ -83,6 +95,5 @@ export const Login = () => {
         </div>
       </div>
     </div>
-
-  )
-}
+  );
+};
